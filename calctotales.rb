467b81dfd1407b3262553aadbdbd801a4aa2dafb
vagrant <vagrant@kleer-csd-box>
2016-08-25 20:24:14 +0200

    calculating taxes for all states

--- calctotales.rb
+++ calctotales.rb
@@ -1,14 +1,28 @@
 class Calctotales
-  def initialize(cant, price)
+  def initialize(cant, price, state)
       @cant = cant.to_i
       @price = price.to_i
+      @state = state
+
+      @state_taxes = {"UT" => 6.85, "NV" => 8.0, "TX" => 6.0, "AL" => 4.0, "CA" => 8.25 }
+  end
+
+  def calcSubtotal
+    @cant * @price
+  end
+
+  def calcSubtotalWithTaxes(subtotal)
+    subtotal * (@state_taxes[@state]/100 + 1)
   end
 
   def showTotal
-    puts "El total sin impuesto es: " + (@cant * @price).to_s
-    puts "El total con impuesto es: " + (@cant * @price * 1.03).to_s
+    subtotal = calcSubtotal
+    subtotalWithTaxes = calcSubtotalWithTaxes(subtotal)
+
+    puts "El total sin impuesto es: " + subtotal.to_s
+    puts "El total con impuesto es: " + subtotalWithTaxes.to_s
   end
 end
 
-program = Calctotales.new(ARGV[0], ARGV[1])
+program = Calctotales.new(ARGV[0], ARGV[1], ARGV[2])
 program.showTotal()
